1b41e03bff218e3527735d8d4b0026ecfed15abf
Vasyl Vasylyk <vasiliyworkplace@gmail.com>
2018-05-14 22:03:01 +0300

    Fix tests

--- src/components/Slot/index.test.js
+++ src/components/Slot/index.test.js
@@ -3,23 +3,30 @@ import Slot from './index';
 import Card from '../Card';
 import { shallow } from 'enzyme';
 
-it.skip('Should create Slot with Card', () => {
-	const slot = shallow(<Slot />);
+it('Should create Slot with Card', () => {
+	const slot = shallow(<Slot iteration={0} />);
+
 	expect(slot.find(Card).length).toBe(1);
 })
 
-it.skip('Should run setNewCard', () => {
+it('Should run setNewCard on first iteration', () => {
+	const mockProps = {
+		setNewCard: jest.fn(),
+		iteration: 1
+	};
+	const slot = shallow(<Slot {...mockProps} />);
+
+	expect(mockProps.setNewCard).toBeCalled();
+})
+
+it('Should not run setNewCard on first iteration', () => {
 	const mockProps = {
 		setNewCard: jest.fn(),
 		iteration: 0
 	};
 	const slot = shallow(<Slot {...mockProps} />);
 
-	slot.setState({
-		iteration: 1
-	})
-
-	expect(mockProps.setNewCard).toBeCalled();
+	expect(mockProps.setNewCard).not.toBeCalled();
 })
 
 it('Should not run setNewCard', () => {
